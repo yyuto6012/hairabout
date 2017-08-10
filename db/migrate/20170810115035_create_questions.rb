45aaf6e2d63f148c245c2da6ff33073ca40f5c8f
yyuto6012 <(yyabu0618@gmail.com)>
2017-08-10 21:05:38 +0900

    created questions table

--- db/migrate/20170810115035_create_questions.rb
+++ db/migrate/20170810115035_create_questions.rb
@@ -1,6 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
+      t.string :title
+      t.text :text
+      t.references :user
+      t.integer :want_count
 
       t.timestamps
     end
