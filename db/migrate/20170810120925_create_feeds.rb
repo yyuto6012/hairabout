35639b66beb4f3d1128e49cef3cd09456659da62
yyuto6012 <(yyabu0618@gmail.com)>
2017-08-10 21:46:46 +0900

    destroy feed model

--- db/migrate/20170810120925_create_feeds.rb
+++
@@ -1,8 +0,0 @@
-class CreateFeeds < ActiveRecord::Migration[5.0]
-  def change
-    create_table :feeds do |t|
-      t.references :feedable, polymorphic: true, index: true
-      t.timestamps
-    end
-  end
-end
--- spec/factories/feeds.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :feed do
-    
-  end
-end
